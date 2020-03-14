017e14fe90b0d79b6cf844e1ff22f0a2375c36d6
ck090 <chandrakanth@Chandras-MacBook-Pro.local>
2020-03-15 01:15:18 +0530

    Changes

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        rText: ''
+    },
+    mounted: function() {
+        this.displayWebPage()
+    },
+    methods: {
+        displayWebPage: function() {
+            function readTextFile(file, callback) {
+                var rawFile = new XMLHttpRequest();
+                rawFile.overrideMimeType("application/json");
+                rawFile.open("GET", file, true);
+                rawFile.onreadystatechange = function() {
+                    if (rawFile.readyState === 4 && rawFile.status == "200") {
+                        callback(rawFile.responseText);
+                    }
+                }
+                rawFile.send(null);
+            }
+            readTextFile("apikey.json", function(text) {
+                var data = JSON.parse(text).key;
+                console.log(data);
+                var http = new XMLHttpRequest()
+                var api_url = "https://api.nasa.gov/planetary/apod?api_key=" + data
+                http.open("GET", api_url)
+                http.send();
+                http.onload = () => {
+                    this.rText = http.responseText;
+                    console.log(rText);
+                }
+            });
         }
     }
-    rawFile.send(null);
-}
-
-readTextFile("apikey.json", function(text) {
-    var data = JSON.parse(text);
-    var key = data.key;
-    console.log(key);
-});
+})
